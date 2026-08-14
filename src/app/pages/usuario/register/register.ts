fbd6a7639e5321c153d454579691c9a38d75dac7
BryanAlexander-Reyes <brayanreyes47k@gmail.com>
2026-08-14 17:28:05 -0500

    se genera html y css del componente de register fecha 14/08/26

--- src/app/pages/usuario/register/register.ts
+++ src/app/pages/usuario/register/register.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+mostrarSegundoFormulario = false;
+
+siguiente(): void {
+  this.mostrarSegundoFormulario = true;
+}
+
+}
