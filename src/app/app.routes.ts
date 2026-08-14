ef3f78e5685f52fde0b124d0a3bbb8c25bc94308
BryanAlexander-Reyes <brayanreyes47k@gmail.com>
2026-08-14 13:40:52 -0500

    se genera rutas para componenter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+// brayan importaciones
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/usuario/login/login';
+import { SpanComponent } from './pages/spancomponent/spancomponent';
+import { RegisterComponent } from './pages/usuario/register/register';
+import { ValidadorComponente } from './pages/validador-componente/validador-componente';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'span',
+        component:SpanComponent
+    },
+    {
+        path:'validador',
+        component:ValidadorComponente
+    },
+];
