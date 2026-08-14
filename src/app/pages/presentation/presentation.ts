fe128532fbe09b336fe1608262fc42c5be32de6f
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-08-14 15:29:25 -0500

    Se agrega contenido a la presentación. A la espera de demás componentes para iniciar la lógica con TypeScript.

--- src/app/pages/presentation/presentation.ts
+++ src/app/pages/presentation/presentation.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-presentation',
-  imports: [],
   templateUrl: './presentation.html',
   styleUrl: './presentation.css',
 })
