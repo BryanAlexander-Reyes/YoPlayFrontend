fa703af46118e15156b72b567c44316e99ee789b
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-08-20 16:12:04 -0500

    Se entrega el desarrollo de la sección estadísticas.

--- src/app/pages/stats/stats.ts
+++ src/app/pages/stats/stats.ts
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
   templateUrl: './stats.html',
   styleUrl: './stats.css',
 })
-export class Stats {}
+export class Stats {
+  promedioJugador = {
+  titulo: 'Tu promedio como jugador',
+  descripcion: '¡Tu desempeño como jugador, en nuestra plataforma, es excelente!',
+
+  resumen: {
+    torneos: 7,
+
+    deportes: [
+      {
+        nombre: 'Voley',
+        cantidad: 4
+      },
+      {
+        nombre: 'Fútbol',
+        cantidad: 2
+      },
+      {
+        nombre: 'Baloncesto',
+        cantidad: 1
+      }
+      ]
+    },
+
+    rendimiento: {
+      torneos: {
+        participados: 7,
+        porcentaje: 42
+      },
+
+      partidos: {
+        jugados: 40,
+        ganados: 26,
+        porcentaje: 65
+      }
+    }
+  };
+
+  // Control del selector "General"
+  mostrarOpciones = false;
+
+  toggleOpciones(): void {
+    this.mostrarOpciones = !this.mostrarOpciones;
+  }
+};
