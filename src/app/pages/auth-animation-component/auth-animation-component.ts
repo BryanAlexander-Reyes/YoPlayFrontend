21bd3932e55a08b246fd390e493708ffa686f97d
BryanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-08-23 12:07:49 -0500

    se genera cambios y se completa modulo de ver torneos

--- src/app/pages/auth-animation-component/auth-animation-component.ts
+++ src/app/pages/auth-animation-component/auth-animation-component.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from '../usuario/login/login';
 import { RegisterComponent } from '../usuario/register/register';
+import {RestablecerComponent} from '../usuario/restablecer-component/restablecer-component'
 
 @Component({
   selector: 'app-auth-animation-component',
-  imports: [CommonModule, FormsModule, LoginComponent, RegisterComponent],
+  imports: [CommonModule, FormsModule, LoginComponent, RegisterComponent, RestablecerComponent],
   templateUrl: './auth-animation-component.html',
   styleUrl: './auth-animation-component.css',
 })
 export class AuthAnimationComponent {
+  mostrarRestablecer=false;
+
+  abrirModal(){
+    this.mostrarRestablecer=true;
+  };
+  cerrarRecuperar(){
+      this.mostrarRestablecer=false;
+    }
+
   registroActivo: boolean = false;
 
   mostrarRegistro(): void {
