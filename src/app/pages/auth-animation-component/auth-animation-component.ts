d69e8318847cecab7a528d8a4bd4cca19ee5cd28
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-08-31 13:10:13 -0500

    Correción de errores finalizado.

--- src/app/pages/auth-animation-component/auth-animation-component.ts
+++ src/app/pages/auth-animation-component/auth-animation-component.ts
@@ -5,16 +5,22 @@ import { LoginComponent } from '../usuario/login/login';
 import { RegisterComponent } from '../usuario/register/register';
 import {RestablecerComponent} from '../usuario/restablecer-component/restablecer-component';
 import { ValidadorComponente } from '../validador-componente/validador-componente';
+import { SpanComponent } from '../spancomponent/spancomponent';
 
 @Component({
   selector: 'app-auth-animation-component',
-  imports: [CommonModule, FormsModule, LoginComponent, RegisterComponent, RestablecerComponent, ValidadorComponente],
+  imports: [CommonModule, FormsModule, LoginComponent, RegisterComponent, RestablecerComponent, ValidadorComponente, SpanComponent],
   templateUrl: './auth-animation-component.html',
   styleUrl: './auth-animation-component.css',
 })
 export class AuthAnimationComponent {
   mostrarRestablecer=false;
   mostrarValidador=false;
+  mostrarCarga = false;
+
+  activarCarga(): void {
+    this.mostrarCarga = true;
+  }
 
   abrirModal(){
     this.mostrarRestablecer=true;
